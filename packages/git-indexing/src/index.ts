35693e1fa6b7367aa5b42f42c4a6029438f47650
Tushar Chhipekar <tusharchhipekar21@gmail.com>
2026-07-20 07:25:57 +0530

    fix rate limit

--- packages/git-indexing/src/index.ts
+++ packages/git-indexing/src/index.ts
@@ -1,2 +1,5 @@
 export { fetchRepoTree } from "./github-fetch";
 export { startIndexing, indexingUsers } from "./pipeline";
+export { getModel } from "./agent/agent.worker";
+export { createRateLimiter } from "./agent/rate-limiter";
+export { fetchFilesContents } from "./github-fetch";
